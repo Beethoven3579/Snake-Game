ac5d354249643353d6cc4ff25377d356172057ae
nickpaolino <nick_paolino@yahoo.com>
2021-05-03 22:38:46 -0400

    start button and refactor

--- app.js
+++ app.js
@@ -8,18 +8,26 @@ let appleY = 400;
 window.onload = () => {
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
-    setInterval(drawEverything, 500);
+    setInterval(() => {
+        drawEverything();
+        moveSnake();
+    }, 400);
 }
 function drawEverything () {
-    snakeHeadX += 15;
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-    canvasContext.fillStyle = 'green';
-    canvasContext.fillRect(snakeHeadX, 200, 25, 25)
+    drawSnake();
     drawApple();
 }
 
 function drawApple () {
     canvasContext.fillStyle = 'red';
-    canvasContext.fillRect(appleX, appleY, 25, 25)
+    canvasContext.fillRect(appleX, appleY, 20, 20)
+}
+function drawSnake() {
+    canvasContext.fillStyle = 'green';
+    canvasContext.fillRect(snakeHeadX, snakeHeadY, 20, 20)
+}
+function moveSnake() {
+    snakeHeadX += 20;
 }
