910693cf9c9d7288830c257adfa923fcee9dcbd0
nickpaolino <nick_paolino@yahoo.com>
2021-05-04 18:54:55 -0400

    snake moves with keypress

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 let canvas;
 let canvasContext;
+canvas = document.getElementById('gameCanvas');
+canvasContext = canvas.getContext('2d');
+document.addEventListener('keydown', handleKeyDown, false);
+
+let rightPressed = false;
+let leftPressed = false;
+let upPressed = false;
+let downPressed = false;
+
 let snakeHeadX = 30;
 let snakeHeadY = 30;
-let appleX = 200;
-let appleY = 400; 
 
-window.onload = () => {
-    canvas = document.getElementById('gameCanvas');
-    canvasContext = canvas.getContext('2d');
+let appleX = Math.floor(Math.random() * canvas.width);
+let appleY = Math.floor(Math.random() * canvas.height);; 
+
+document.getElementById('start').addEventListener('click', () => {
+    document.querySelector('#start').disabled = true;
     setInterval(() => {
         drawEverything();
-        moveSnake();
+        detectWallCollisions();
     }, 400);
-}
+ 
+})
+
 function drawEverything () {
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
@@ -27,7 +38,56 @@ function drawApple () {
 function drawSnake() {
     canvasContext.fillStyle = 'green';
     canvasContext.fillRect(snakeHeadX, snakeHeadY, 20, 20)
+    document.addEventListener('keydown', handleKeyDown, false);
+
+    if(rightPressed) {
+        snakeHeadX += 20;
+    }
+    else if(leftPressed) {
+        snakeHeadX -= 20;
+    }
+    if(downPressed) {
+        snakeHeadY += 20;
+    }
+    else if(upPressed) {
+        snakeHeadY -= 20;
+    }
 }
-function moveSnake() {
-    snakeHeadX += 20;
+function detectWallCollisions() {
+    if (snakeHeadX > canvas.width || snakeHeadX < 0
+        || snakeHeadY > canvas.height || snakeHeadY <= 0) {
+        alert('Game Over');
+        playerScore = 0;
+        rightPressed = false;
+        leftPressed = false;
+        upPressed = false;
+        downPressed = false;
+    }
+}
+
+function handleKeyDown(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+        leftPressed = false;
+        upPressed = false;
+        downPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+        rightPressed = false;
+        upPressed = false;
+        downPressed = false;
+    }
+    if(e.keyCode == 40) {
+        downPressed = true;
+        rightPressed = false;
+        leftPressed = false;
+        upPressed = false;
+    }
+    else if(e.keyCode == 38) {
+        upPressed = true;
+        rightPressed = false;
+        leftPressed = false;
+        downPressed = false;
+    }
 }
